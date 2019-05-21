1024140bb51ed944806e47532aca4ad95f9b14f4
hsrsbd <hsraffety@gmail.com>
2019-05-20 20:35:52 -0700

    updating js. creating Articles class.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
 console.log(`index.js linked`);
+class Article {
+  constructor(article) {
+    this.article = article;
+    this.data = this.article.dataset.article;
+    this.articleElement = document.querySelector(
+      `.show-more[data-article="${this.data}"]`
+    );
+    this.article.addEventListener('click', () => this.showArticle());
+  }
+
+  showArticle() {
+    const indArticle = document.querySelectorAll('.article-content');
+    Array.from(indArticle).forEach(el => {
+      el.classList.remove('show');
+    });
+    console.log(`clicked`);
+  }
+}
+
+const articles = document.querySelectorAll('.article-content');
+
+articles.forEach(article => {
+  new Article(article);
+});
