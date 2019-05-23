a45efb34676e97bf89803f3f9f2f47b9e881f083
hsrsbd <hsraffety@gmail.com>
2019-05-22 17:17:57 -0700

    updates.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,23 @@
-class ArticleButton {
+class ArticleTitle {
   constructor(element) {
     this.element = element;
     this.data = this.element.dataset.article;
     this.itemElement = document.querySelector(
       `.article-content[data-article='${this.data}']`
     );
-    console.log(`item element`, this.itemElement);
+    // console.log(`item element`, this.itemElement);
     this.articleItem = new ArticleItem(this.itemElement);
-    console.log(`article item`, this.articleItem);
-    this.element.addEventListener('click', () => this.select());
+    console.log(`new ArticleItem`, this.articleItem);
+    this.element.addEventListener('click', () => this.selected());
+    console.log(`this.element`, this.element);
   }
-  select() {
-    const articles = document.querySelectorAll('.article-content');
-    Array.from(articles).forEach(article => {
-      article.classList.remove('show');
-    });
-    this.element.classList.add('show');
+  selected() {
+    console.log(`'this' inside of selected()`, this);
+    // const articles = document.querySelectorAll('.article-content');
+    // Array.from(articles).forEach(article => {
+    //   article.classList.remove('show');
+    // });
+    // this.element.classList.add('show');
     this.articleItem.select();
   }
 }
@@ -23,18 +25,20 @@ class ArticleButton {
 class ArticleItem {
   constructor(element) {
     this.element = element;
+    console.log(`this.element inside ArticleItem constructor`, this.element);
   }
   select() {
+    console.log(`'this' inside of select()`, this);
     const articles = document.querySelectorAll('.article-content');
     Array.from(articles).forEach(article => {
-      article.classList.remove('show');
+      article.classList.remove('show', 'fadeIn');
     });
     this.element.classList.add('show', 'fadeIn');
   }
 }
 
-let articles = document.querySelectorAll('.article-title');
+let articleTitles = document.querySelectorAll('.article-title');
 
-articles.forEach(article => {
-  new ArticleButton(article);
+articleTitles.forEach(articleTitle => {
+  new ArticleTitle(articleTitle);
 });
